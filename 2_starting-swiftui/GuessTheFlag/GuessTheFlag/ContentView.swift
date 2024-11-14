d25927dba3bf064c94b4c7bfa93f8d512c658c7d
Cameron Browning <camb416@gmail.com>
2024-11-13 19:04:07 -0500

    finished guess the flag styling

--- 2_starting-swiftui/GuessTheFlag/GuessTheFlag/ContentView.swift
+++ 2_starting-swiftui/GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -22,14 +22,26 @@ struct ContentView: View {
             RadialGradient(stops: [
                 .init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
                 .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3),
-            ], center: .top, startRadius: 200, endRadius: 400)
+            ], center: .top, startRadius: 200, endRadius: 700)
             .ignoresSafeArea()
+            
             VStack{
+                Spacer()
+         Text("Guess the Flag")
+                    .foregroundStyle(.white)
+                    .font(.largeTitle.weight(.bold))
+            
+            VStack(spacing: 15){
                 
-                VStack(spacing: 15){
-                    Text("Tap the flag of").font(.subheadline.weight(.heavy))
-                    Text(countries[correctAnswer]).font(.largeTitle.weight(.semibold))
+                VStack(){
+                    Text("Tap the flag of")
+                        .foregroundStyle(.secondary)
+                        .font(.subheadline.weight(.heavy))
+                    Text(countries[correctAnswer])
+                        .font(.largeTitle.weight(.semibold))
+                    
                 }
+                
                 ForEach(0..<3) { number in
                     Button {
                         flagTapped(number)
@@ -39,6 +51,22 @@ struct ContentView: View {
                     }
                 }
             }
+            
+            .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                Spacer()
+                Spacer()
+                Text("Score: ???")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                Spacer()
+                Spacer()
+            }
+            .padding()
+            
+       
             
         }.alert(scoreTitle, isPresented: $showingScore){
             Button("Continue", action: askQuestion)
